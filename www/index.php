ba683bca702405761da2ef0133918ac946fc4606
olvit <olvitov@gmail.com>
2016-05-11 23:18:23 +0300

    1:00

--- www/index.php
+++ www/index.php
@@ -4,19 +4,43 @@ class Article {
 
     public $title;
     public $text;
+    public $user;
+
+    public function __construct($title , $text, $user = '')
+    {
+        $this->title = $title;
+        $this->text = $text;
+        $this->user = $user;
+
+    }
+
+
     public function view() {
         echo $this->title;
+        echo $this->text;
+    }
+
+    public function index() {
+
+       echo $this->user;
     }
 }
 
-$a = new Article();
+$a = new Article('Заголовок новости ', 'Текст новости');
 
 
-$a->title = 'Заголовок статьи';
-var_dump($a);
+// $a->title = ' Заголовок статьи_1';
+$a->text = ' Текст новости_1';
+// var_dump($a);
 $a->view();
 
-$b = new Article();
-$b->title = 'Другой заголовок';
+ $b = new Article('Заголовок новости2', 'Текст новости2');
+// $b->title = 'Другой заголовок ';
 echo '<br>';
-$b->view();
+ $b->view();
+
+$c = new Article('', '', 'olvit');
+echo '<br>';
+$c->index();
+
+
